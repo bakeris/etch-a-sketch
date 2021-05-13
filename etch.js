67c86bfa61c01ca8349ff426afdf555ead6388a2
Tomas Meskonis <Tomas.meskonis@gmail.com>
2021-05-13 18:31:15 +0100

    added comments

--- etch.js
+++ etch.js
@@ -1,11 +1,12 @@
 
 const container = document.querySelector('#container');
+//loops trough grid and creates divs
 for (let i=0; i<256; i++){
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square)
 };
-
+//gives square a color whwn mouse enters a square 
 const sqrs = document.querySelectorAll('.square')
 sqrs.forEach(square => {
     square.addEventListener('mouseenter',(event) => {
