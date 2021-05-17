dd45e09d210581b43912ae03795fa46617a576de
Tomas Meskonis <Tomas.meskonis@gmail.com>
2021-05-17 21:23:24 +0100

    added css styles

--- etch.js
+++ etch.js
@@ -1,7 +1,10 @@
 
-const container = document.querySelector('#container');
+const container = document.querySelector('#mainContainer');
+let cNum = 15;
+let rNum = 15;
+let sqNum = cNum * rNum;
 //loops trough grid and creates divs
-for (let i=0; i<256; i++){
+for (let i=0; i<sqNum; i++){
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square)
@@ -12,9 +15,16 @@ sqrs.forEach(square => {
     square.addEventListener('mouseenter',(event) => {
         event.target.style.backgroundColor = 'red';
         setTimeout(function() {
-            event.target.style.backgroundColor = "";
+            event.target.style.backgroundColor = "red";
           }, 500);
         });
 });
 
+const grid = document.getElementById('#mainContainer');
 
+const clear = document.getElementById('#clear');
+
+
+
+let columns = container.style.setProperty('grid-template-columns', 'repeat(' + cNum + ', 1fr)');;
+let rows = container.style.setProperty('grid-template-columns', 'repeat(' + rNum + ', 1fr)');
