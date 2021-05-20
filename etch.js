a1b81e17cfef57e3839d74e71e3d806c2a06f703
Tomas Meskonis <Tomas.meskonis@gmail.com>
2021-05-20 17:38:23 +0100

    finished project

--- etch.js
+++ etch.js
@@ -1,30 +1,74 @@
 
 const container = document.querySelector('#mainContainer');
-let cNum = 15;
-let rNum = 15;
-let sqNum = cNum * rNum;
-//loops trough grid and creates divs
-for (let i=0; i<sqNum; i++){
+const clear = document.getElementById('clear');
+clear.addEventListener('click',clearSqr)
+const sqrs = document.querySelectorAll('.square')
+const colorValue = document.getElementById('favcolor').value
+const colorButton = document.getElementById('favcolor')
+colorButton.addEventListener('input',changeColor)
+const remove = document.getElementById('delete')
+remove.addEventListener('click',restart)
+
+
+//default window
+let defCol = 20
+let defRow = 20
+let defNum = defCol * defRow;
+for (let i=0; i<defNum; i++){
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square)
+    changeColor ()
+};
+
+
+//gives square a color whwn mouse enters a square 
+function changeColor () {
+    const sqrs = document.querySelectorAll('.square')
+    const colorValue = document.getElementById('favcolor').value
+    sqrs.forEach(square => {
+        square.addEventListener('mouseenter',(event) => {
+            event.target.style.backgroundColor = colorValue;
+            });
+    });
+};
+
+
+
+// this function clear all squares background
+function restart () {
+    const restart = document.getElementsByClassName('square')
+   for (let i = restart.length-1; i>=0; i--) {
+       restart[i].style.backgroundColor = 'black';
+   }
+};
+
+// this is clear suqares function
+function clearSqr () {
+    //function that clears all squares
+   const clearSqr = document.getElementsByClassName('square')
+   for (let i = clearSqr.length-1; i>=0; i--) {
+       clearSqr[i].remove();
+   }
+   //function that creates new squares
+    let sqrNum = prompt('Enter number of squares between 4-64')
+    if (sqrNum > 64 || sqrNum < 4 || Number.isNaN(sqrNum)) {
+        alert('Enter a number from 4-64 range')
+    } else {
+    let newNum = sqrNum * sqrNum
+    for (let i=0; i<newNum; i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square)
+    changeColor()
 };
 //gives square a color whwn mouse enters a square 
-const sqrs = document.querySelectorAll('.square')
-sqrs.forEach(square => {
-    square.addEventListener('mouseenter',(event) => {
-        event.target.style.backgroundColor = 'red';
-        setTimeout(function() {
-            event.target.style.backgroundColor = "red";
-          }, 500);
-        });
-});
+let columns = container.style.setProperty('grid-template-columns', 'repeat(' + sqrNum + ', 1fr)');
+let rows = container.style.setProperty('grid-template-rows', 'repeat(' + sqrNum + ', 1fr)');
+  };
+};
 
-const grid = document.getElementById('#mainContainer');
+// this is clear button with added function to add and remove squares
 
-const clear = document.getElementById('#clear');
-
-
-
-let columns = container.style.setProperty('grid-template-columns', 'repeat(' + cNum + ', 1fr)');;
-let rows = container.style.setProperty('grid-template-columns', 'repeat(' + rNum + ', 1fr)');
+let columns = container.style.setProperty('grid-template-columns', 'repeat(' + defCol + ', 1fr)');
+let rows = container.style.setProperty('grid-template-rows', 'repeat(' + defRow + ', 1fr)');
